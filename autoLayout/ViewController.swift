227d8cef29ccb65667b4eb56c5e4b2e3b7bdf1ef
somsak <somsak.kaeworasan@outlook.com>
2021-05-30 15:28:03 +0700

    First commit.

---
+++ autoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by somsak on 30/5/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
